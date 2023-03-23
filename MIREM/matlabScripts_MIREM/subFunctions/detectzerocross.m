bdbc8e637275c87a618059772722c7e8a256f52b
Eliot LOUYS <eliot.louys@grenoble-inp.org>
2023-03-23 17:14:50 +0100

    adding description of the way detectcross works

--- MIREM/matlabScripts_MIREM/subFunctions/detectzerocross.m
+++ MIREM/matlabScripts_MIREM/subFunctions/detectzerocross.m
@@ -1,4 +1,16 @@
 function [zerocross, set]  = detectzerocross(x)
+    % computes the zerocrossing indexes and if the crossing is an
+    % onset/offset. 
+    % Values returned :
+    % zerocross : a vector of the indexes in the vector x right before the
+    % zero-crossing happens.
+    % 
+    % set : return a vector the same length as zerocross. This vector is
+    % only composed of 1 and -1. It is related to the zerocross vector, 1
+    % means this crossing is an onset crossing and -1 means it's an offset
+    % crossing
+
+
     idx                    = [1:length(x)];
     last                   = circshift(x,1);
     zerocross              = idx(x.*last<0);  %  = TRUE when x crosses zero
